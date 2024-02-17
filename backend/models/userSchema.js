29fe0da00ea762ef8c32381259cbea5688a0ccc5
Ravin Yadav <ravinyadav973@gmail.com>
2024-02-17 16:32:31 +0530

    auth  schema created

---
+++ backend/models/userSchema.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    role: {
+        type:String,
+        require:[true, 'role is require'],
+        enum:['admin', 'organisation', 'user', 'hospital']
+    },
+    name:{
+        type :String,
+        require: function() {
+            if(this.role === 'user' || this.role === 'admin'){
+                return true
+            }
+            return false
+        }
+    },
+    organisationName: {
+        type: String,
+        require:function(){
+           if(this.role === 'organisation') {
+            return true
+           }
+           return false
+        }
+    },
+    hospitalName: {
+        type: String,
+        require: function() {
+            if(this.role === 'hospital') {
+                return true
+            }
+            return false
+        }
+    },
+    email: {
+        type: String,
+        require:[true, 'email is require'],
+        unique: true
+    },
+    password:{
+        type: String,
+        require:[true, 'password is require']
+    },
+    website:{
+        type: String
+    },
+    address: {
+        type: String,
+        required: [true, "Address is Required"],
+    },
+    phone:{
+        type:String,
+        required: [true, 'Phone No is required']
+    }
+}, {timestamps:true})
+
+const User = mongoose.model("Users", userSchema);
+
+export default User;
